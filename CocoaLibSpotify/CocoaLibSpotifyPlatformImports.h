9b028232656f2e422fa2beb05df92f7ac78cec0b
Daniel Kennett <dan@spotify.com>
2011-08-25 22:56:11 +0200

    Rearranged project directory structure to make more sense now we're multi platform.

--- CocoaLibSpotify/CocoaLibSpotifyPlatformImports.h
+++
@@ -1,9 +0,0 @@
-#if TARGET_OS_IPHONE
-#import "api.h"
-#import <UIKit/UIKit.h>
-#define SPPlatformNativeImage UIImage
-#else
-#import <Cocoa/Cocoa.h>
-#import <libspotify/api.h>
-#define SPPlatformNativeImage NSImage
-#endif
