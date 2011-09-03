91baa604dd7e6a79fe1c159da1a40daa48416738
Adrien BRAULT <monsti@gmail.com>
2011-09-03 17:52:23 +0200

    Fix syntax error

--- common/CocoaLibSpotify.h
+++ common/CocoaLibSpotify.h
@@ -51,7 +51,7 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #import "SPArtistBrowse.h"
 #import "SPAlbumBrowse.h"
 #import "SPToplist.h"
-#import "SPRadioSearch.h
+#import "SPRadioSearch.h"
 
 #else
 
